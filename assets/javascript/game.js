301644c552d04641b01515025d0841b46a1630e9
Monica Amitin <monzielvsu@gmail.com>
2019-10-16 18:04:17 -0700

    added code to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,27 @@
 // create var's
+var usersToral= 0;
+var wins= 0;
+var losses= 0;
 
+//create generated target guess between 19-120 to start game
+$( document ).ready(function){
+    var targetNum=Math.floor(Math.random())
+}
+//generate score for crystals 1-4 with values between 1-12
+
+var gemOne= Math.floor(Math.random)
+var gemTwo= Math.floor(Math.random)
+var gemThree= Math.floor(Math.random)
+var gemFour= Math.floor(Math.random)
+
+//capture users click on crystals
+
+//display score of crystals
+
+//add score of crystals to users total
+
+//if users score doesnt equal target repeat click on crystals
+
+//if users total euqals to target wins go up and game starts over
+
+//if users total goes over target number the losses go up and game starts over
