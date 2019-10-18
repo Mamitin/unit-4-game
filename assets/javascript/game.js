f6e76be544f116b5623e92b9d67558df75aae5f6
Amitin <MAmitin@academyart.edu>
2019-10-18 12:40:50 -0700

    cleaned up reset game to startGame function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,42 @@
-//create generated target guess between 19-120 to start game
-$(document).ready(function () {
-    genTarget();
-})
-
-function genTarget() {
-    targetNum = Math.floor(Math.random() * 101 + 19);
-    $('.numToMatch').text(targetNum);
-}
-
-
 // create var's
 var usersTotal = 0;
 var wins = 0;
 var losses = 0;
 var targetNum = 0;
 
+var gemOne = 0;
+var gemTwo = 0;
+var gemThree = 0;
+var gemFour = 0;
 
-//generate score for crystals 1-4 with values between 1-12
-//capture users click on crystals
+//Start game after page finishes loading
+$(document).ready(function () {
+    startGame();
+})
 
-var gemOne = Math.floor(Math.random() * 11 + 1)
-var gemTwo = Math.floor(Math.random() * 11 + 1)
-var gemThree = Math.floor(Math.random() * 11 + 1)
-var gemFour = Math.floor(Math.random() * 11 + 1)
+//create generated target guess between 19-120 to start game
+function genTarget() {
+    targetNum = Math.floor(Math.random() * 101 + 19);
+    $('.numToMatch').text(targetNum);
+}
+
+//start/reset function to start & restart game after win or lose
+function startGame() {
+    genTarget();
+    //generate score for crystals 1-4 with values between 1-12
+    gemOne = Math.floor(Math.random() * 11 + 1);
+    gemTwo = Math.floor(Math.random() * 11 + 1);
+    gemThree = Math.floor(Math.random() * 11 + 1);
+    gemFour = Math.floor(Math.random() * 11 + 1);
+    
+    usersTotal = 0;
+    $('#youTotal').text(usersTotal);
+}
+
 
 function updateTotal (userGuess) {
     //add score of crystals to users total
-    //add usersTotal and gemOne
+    //add usersTotal and usersGuess
     usersTotal = usersTotal + userGuess;
     console.log("New usersTotal= " + usersTotal);
     //display the new sum
@@ -65,7 +75,7 @@ function win() {
     wins++;
     $('#wins').text("Wins: " + wins);
     alert("You're a winner!");
-    reset();
+    startGame();
 }
 
 //create function for if user losses game
@@ -73,17 +83,5 @@ function loser() {
     losses++;
     $('#losses').text("Losses: " + losses);
     alert("Sorry! Better luck next time");
-    reset();
-}
-
-//reset function to restart game after win or lose
-function reset() {
-    genTarget();
-    gemOne = Math.floor(Math.random() * 11 + 1);
-    gemTwo = Math.floor(Math.random() * 11 + 1);
-    gemThree = Math.floor(Math.random() * 11 + 1);
-    gemFour = Math.floor(Math.random() * 11 + 1);
-
-    usersTotal = 0;
-    $('#youTotal').text(usersTotal);
+    startGame();
 }
